057b9b96259bd6a4fcc67cc7e2f2588639d6b4fd
ShashankAnand1996 <shashankkumaranand@gmail.com>
2017-02-28 04:06:16 +0530

    Update ideone_FkmHOL.cpp

--- ideone_FkmHOL.cpp
+++ ideone_FkmHOL.cpp
@@ -1,41 +1,57 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 // hort[i][j]= 0 if there is bad path between (i-1,j) to (i,j) else 1
 // vert[i][j]= 0 if there is bad path between (i,j-1) to (i,j) else 1
 
-int path(int m, int n)
+long path(int n, int m, string s[],int t)
 {
      int i, j;
-     
-     int tp[m][n];  
-     int hort[m][n];
-     int vert[m][n];
+     long long tp[m+1][n+1];  
+     long long hort[m+1][n+1];
+     long long vert[m+1][n+1];
  
-     tp[0][0] = 0;
+     tp[0][0] = 1;
      
-     for (i = 1; i <= m; i++)
+     for (i = 0; i <= m; i++)
      {
-          for (j = 1; j <= n; j++)
+          for (j = 0; j <= n; j++)
           { 
                 vert[i][j] = 1;
                 hort[i][j] = 1;
           }
      }    
+     for(int i=0;i<t;i++){
+        int a,b,c,d;
+	string word = s[i];
+	string str = " ";
+	a  = atoi((word.substr(0,word.find(str))).c_str());
+	word.erase(0,(word.substr(0,word.find(str))).length()+1);
+	b = atoi((word.substr(0,word.find(str))).c_str());
+	word.erase(0,(word.substr(0,word.find(str))).length()+1);
+	c = atoi((word.substr(0,word.find(str))).c_str());
+	word.erase(0,(word.substr(0,word.find(str))).length()+1);
+	d = atoi((word.substr(0,word.find(str))).c_str());
+	cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+	if(a==c){
+		vert[d][a] = 0;
+	}
+	else{
+		hort[b][c] = 0;
+	}
+     }
      for (i = 1; i <= m; i++)
      {
-          for (j = 1; j <= i; j++)
-          { 
-               if(hori[j][0]==0)
-        	     { tp[i][0] = 1;}
-          }
+          if(vert[i][0]==0)
+		tp[i][0] = 0;
+	  else
+		tp[i][0] = tp[i-1][0];
      }
-      for (j = 1; i <= n; j++)
+      for (j = 1; j <= n; j++)
      {
-          for (i = 1; i <= j; i++)
-          { 
-               if(vert [0][0]==0)
-        	     { tp[i][0] = 1;}
-          }
+          if(hort[0][j]==0)
+		tp[0][j] = 0;
+	  else
+		tp[0][j] = tp[0][j-1];
      }
      
      for (i = 1; i <= m; i++)
@@ -57,13 +73,13 @@ int path(int m, int n)
  
 
 /* Driver program to test above functions */
-int main()
-{ int width, height;
- cin>>width;
- cin>>height;
-   int bad[][] = { {1, 2, 3},
-                      {4, 8, 2},
-                      {1, 5, 3} };
-   printf(" %d ", path(width, height));
-   return 0;
+int main(){
+int w = 6;
+int h = 6;
+string s[] = {"0 0 0 1","5 6 6 6"};
+int n = sizeof(s)/sizeof(s[0]);
+//cout<<n<<endl;
+int result = path(w,h,s,n);
+cout<<result<<endl;
+return 0;
 }
