0a6be2cec1eba624a366870b2b5b45315d5a5a7f
ShashankAnand1996 <shashankkumaranand@gmail.com>
2017-02-28 01:12:31 +0530

    Update ideone_FkmHOL.cpp

--- ideone_FkmHOL.cpp
+++ ideone_FkmHOL.cpp
@@ -1,38 +1,69 @@
 #include <iostream>
 using namespace std;
-int path(int cost[R][C], int m, int n)
+// hort[i][j]= 0 if there is bad path between (i-1,j) to (i,j) else 1
+// vert[i][j]= 0 if there is bad path between (i,j-1) to (i,j) else 1
+
+int path(int m, int n)
 {
      int i, j;
      
-     int tp[R][C];  
+     int tp[m][n];  
+     int hort[m][n];
+     int vert[m][n];
  
-     tc[0][0] = cost[0][0];
+     tp[0][0] = 0;
      
      for (i = 1; i <= m; i++)
-        if(hori[i][0]==1)
-        	{ tp[i][0] = 1;}
- 
-     for (j = 1; j <= n; j++)
-        {
-        	if(vert[0][j]==1)
-        	{ tp[0][j] = 1;}
-        	
-        }
-     /* Construct rest of the tp array */
+     {
+          for (j = 1; j <= n; j++)
+          { 
+                vert[i][j] = 1;
+                hort[i][j] = 1;
+          }
+     }    
      for (i = 1; i <= m; i++)
-        for (j = 1; j <= n; j++)
-            tp[i][j] = tp[i-1][j-1]+tp[i-1][j];
- 
+     {
+          for (j = 1; j <= i; j++)
+          { 
+               if(hori[j][0]==0)
+        	     { tp[i][0] = 1;}
+          }
+     }
+      for (j = 1; i <= n; j++)
+     {
+          for (i = 1; i <= j; i++)
+          { 
+               if(vert [0][0]==0)
+        	     { tp[i][0] = 1;}
+          }
+     }
+     
+     for (i = 1; i <= m; i++)
+     {
+          for (j = 1; j <= n; j++)
+          {
+                if(hort[i][j]==0 && vert[i][j]==0)
+                { tp[i][j]=0; }
+               else if(hort[i][j]==0)
+                { tp[i][j] = tp[i][j-1]; }
+               else if(vert[i][j]==0)
+                { tp[i][j] = tp[i-1][j]; }
+               else 
+                { tp[i][j]=tp[i][j-1] + tp[i-1][j]; }
+          }
+      }     
      return tp[m][n];
 }
  
 
 /* Driver program to test above functions */
 int main()
-{
-   int cost[R][C] = { {1, 2, 3},
+{ int width, height;
+ cin>>width;
+ cin>>height;
+   int bad[][] = { {1, 2, 3},
                       {4, 8, 2},
                       {1, 5, 3} };
-   printf(" %d ", path(cost, 2, 2));
+   printf(" %d ", path(width, height));
    return 0;
 }
